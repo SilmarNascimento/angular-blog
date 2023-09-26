f988f48d3438206f2658ee82fd6083d53bbe6687
SilmarNascimento <silmarfnascimento@gmail.com>
2023-09-26 11:12:57 -0300

    feat: organiza alguns componentes para o layout do blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+import { SideCardComponent } from './components/side-card/side-card.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContentComponent,
+    SideCardComponent,
+    MainCardComponent,
+    MenuBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/side-card/side-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-card',
+  templateUrl: './side-card.component.html',
+  styleUrls: ['./side-card.component.css']
+})
+export class SideCardComponent {
+
+}
