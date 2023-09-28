2fe73e7fb8d3410b8f206184d4bbeb48f881b5c4
SilmarNascimento <silmarfnascimento@gmail.com>
2023-09-28 20:06:07 -0300

    feat: adiciona a funcionalidade de resize na textArea

--- src/app/components/text-area/text-area.component.ts
+++ src/app/components/text-area/text-area.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
   templateUrl: './text-area.component.html',
   styleUrls: ['./text-area.component.css']
 })
-export class TextAreaComponent {
+export class TextAreaComponent implements OnInit{
 
+  constructor() { }
+
+  ngOnInit(): void {
+    const textarea = document.getElementById('add-comment');
+    if(textarea) {
+      textarea.addEventListener('input', () => {
+        textarea.style.height = 'auto';
+        textarea.style.height = `${textarea.scrollHeight}px`;
+      });
+    }
+  }
 }
